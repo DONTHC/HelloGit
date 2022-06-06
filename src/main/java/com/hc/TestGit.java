71b0631afffa100dd53b8bf4ba8a7f64ec2f6c78
HCong <2460865183@qq.com>
2022-06-06 21:01:15 +0800

    03-HelloGit-Commit

--- src/main/java/com/hc/TestGit.java
+++ src/main/java/com/hc/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello!!!");
         System.out.println("Hello HC!!");
+        System.out.println("Hello HC!!!");
     }
 }
